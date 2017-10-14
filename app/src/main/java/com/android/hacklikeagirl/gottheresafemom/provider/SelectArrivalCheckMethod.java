0d66960693ba24908af6163d9d5f8f4ad257f2cc
pascha <s0554193@htw-berlin.de>
2017-10-14 12:30:02 -0400

    Popup with the time picker added

--- app/src/main/java/com/android/hacklikeagirl/gottheresafemom/provider/SelectArrivalCheckMethod.java
+++ app/src/main/java/com/android/hacklikeagirl/gottheresafemom/provider/SelectArrivalCheckMethod.java
@@ -2,16 +2,59 @@ package com.android.hacklikeagirl.gottheresafemom.provider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TimePicker;
 
 import com.android.hacklikeagirl.gottheresafemom.R;
 
 public class SelectArrivalCheckMethod extends AppCompatActivity {
 
+    private TimePicker timePicker1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_arrival_check_method);
+
+    final Button button = (Button) findViewById(R.id.button_determine_by_time);
+         button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            onButtonShowPopupWindowClick(v);
+        }
+    });
+
+        timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
     }
 
+    public void onButtonShowPopupWindowClick(View view) {
+
+        // get a reference to the already created main layout
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.activity_select_arrival_check_method);
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.pick_time_layout, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        /*final Button setTimeButton = (Button) findViewById(R.id.button_select_time);
+        setTimeButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });*/
+
+        // show the popup window
+        popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
+
+
+    }
 }
