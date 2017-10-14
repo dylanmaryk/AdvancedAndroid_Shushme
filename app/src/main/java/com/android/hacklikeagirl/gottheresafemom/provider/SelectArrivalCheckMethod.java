cac182659f7c66be55d17a0efe65bed3da1faa51
Anna Morgiel <morgiel@uni-potsdam.de>
2017-10-14 18:09:33 +0200

    add ClickListeners for the buttons

--- app/src/main/java/com/android/hacklikeagirl/gottheresafemom/provider/SelectArrivalCheckMethod.java
+++ app/src/main/java/com/android/hacklikeagirl/gottheresafemom/provider/SelectArrivalCheckMethod.java
@@ -1,20 +1,55 @@
 package com.android.hacklikeagirl.gottheresafemom.provider;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.android.hacklikeagirl.gottheresafemom.MainActivity;
 import com.android.hacklikeagirl.gottheresafemom.R;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class SelectArrivalCheckMethod extends AppCompatActivity {
 
+    private Button location = (Button) findViewById(R.id.button_determine_by_location);
+    private Button flightNumber = (Button) findViewById(R.id.button_determine_by_flightnr);
+    private Button time = (Button) findViewById(R.id.button_determine_by_time);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_arrival_check_method);
+
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(SelectArrivalCheckMethod.this, MainActivity.class);
+                SelectArrivalCheckMethod.this.startActivity(myIntent);
+            }
+        });
+
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 14.10.2017  go to Pick Time Activity
+                Intent myIntent = new Intent(SelectArrivalCheckMethod.this, MainActivity.class);
+                SelectArrivalCheckMethod.this.startActivity(myIntent);
+            }
+        });
+
+        flightNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 14.10.2017  go to Pick Flight Number Activity
+                Intent myIntent = new Intent(SelectArrivalCheckMethod.this, MainActivity.class);
+                SelectArrivalCheckMethod.this.startActivity(myIntent);
+            }
+        });
     }
+
 
     @Override
     protected void attachBaseContext(Context newBase) {
