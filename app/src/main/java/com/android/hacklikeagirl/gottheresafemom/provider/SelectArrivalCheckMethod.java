da9fc5e324b6de0cd4719b1ef77624b115284b61
Anna Morgiel <morgiel@uni-potsdam.de>
2017-10-14 17:33:26 +0200

    delete unnecessary class

--- app/src/main/java/com/android/hacklikeagirl/gottheresafemom/provider/SelectArrivalCheckMethod.java
+++ app/src/main/java/com/android/hacklikeagirl/gottheresafemom/provider/SelectArrivalCheckMethod.java
@@ -1,9 +1,12 @@
 package com.android.hacklikeagirl.gottheresafemom.provider;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.android.hacklikeagirl.gottheresafemom.R;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class SelectArrivalCheckMethod extends AppCompatActivity {
 
@@ -11,5 +14,10 @@ public class SelectArrivalCheckMethod extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_arrival_check_method);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
