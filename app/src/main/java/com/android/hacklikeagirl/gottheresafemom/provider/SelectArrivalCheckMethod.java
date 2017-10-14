72b2c329f53206838d1ec8f875ef973894a79e8d
pascha <s0554193@htw-berlin.de>
2017-10-14 11:13:14 -0400

    small layout changes

--- app/src/main/java/com/android/hacklikeagirl/gottheresafemom/provider/SelectArrivalCheckMethod.java
+++ app/src/main/java/com/android/hacklikeagirl/gottheresafemom/provider/SelectArrivalCheckMethod.java
@@ -2,6 +2,7 @@ package com.android.hacklikeagirl.gottheresafemom.provider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.android.hacklikeagirl.gottheresafemom.R;
 
@@ -12,4 +13,5 @@ public class SelectArrivalCheckMethod extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_arrival_check_method);
     }
+
 }
